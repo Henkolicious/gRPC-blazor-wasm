327b1a80447540920f5b4e6c772c1ce7c957b952
Henkolicious <me@henriklarsson.eu>
2020-05-21 10:07:38 +0200

    init

---
+++ readme.md
@@ -0,0 +1,12 @@
+# gRPC Blazor WebAssembly
+
+Very basic example.
+Generated from CLI `dotnet new blazorwasm -n gRPC --hosted`
+
+## RUN
+
+### CLI
+src/Server `dotnet run`
+
+### VS Studio
+Set Server as startup project.
---
+++ src/Server/Services/GreeterService.cs
@@ -0,0 +1,22 @@
+﻿using Grpc.Core;
+using GrpcGreeter;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+public class GreeterService : Greeter.GreeterBase
+{
+    private readonly ILogger<GreeterService> _logger;
+
+    public GreeterService(ILogger<GreeterService> logger)
+    {
+        _logger = logger;
+    }
+    
+    public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+    {
+        _logger.LogInformation($"Request: {@request}");
+        var reply = new HelloReply { Message = $"Hello from gRPC, {request.Name}!" };
+        _logger.LogInformation($"Reply: {@reply}");
+        return Task.FromResult(reply);
+    }
+}
